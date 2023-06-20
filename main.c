13b4a35d4b6210fe6819a6823c25c6f969739871
Edward Garcia <edward.g2141@gmail.com>
2023-06-19 17:29:36 -0700

    Added file header with description and history

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+/****************************************************************************
+ Module
+     main.c
+ Description
+     starter code for STM32 firware
+ Notes
+
+ History
+ When           Who     What/Why
+ -------------- ---     --------
+ 06/18/23 5:30 Edward   started coding
+*****************************************************************************/
+
+
 * (volatile uint32_t *) (0x42020000 + 0) = 0;	// Sets pins A0 - A15 to inputs
 
 * (volatile uint32_t *) (0x42020000 + 0) &= 3 << 6;	// Clear bit ranges 6-7
