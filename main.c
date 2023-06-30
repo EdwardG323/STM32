2e42ab1091c1609ba48aa6c5cd9c65f9f0f1b35b
Edward <edward.g2141@gmail.com>
2023-06-30 07:37:17 -0700

    Implemented makefile

--- main.c
+++ main.c
@@ -10,62 +10,82 @@
  -------------- ---     --------
  06/18/23 5:30 Edward   started coding
 *****************************************************************************/
-#include <inttypes.h>
-#include <stdbool.h>
+// #include <inttypes.h>
+// #include <stdbool.h>
 
-struct gpio {
-    volatile uint32_t MODER, OTYPER, OSPEEDER, PUPDR, IDR, ODR, BSRR, LCKR, AFR[2];
-};
-
-
-#define GPIOA ((struct gpio *) 0x42020000)
-#define GPIO(bank) ((struct gpio *) (0x42020000 + 0x400 * (bank)))
-
-#define BIT(x) (1UL << (x))
-#define PIN(bank, num) ((((bank) - 'A') << 8) | (num))
-#define PINNO(pin) (pin & 255)
-#define PINBANK(pin) (pin >> 8) 
-
-enum {
-    GPIO_MODE_INPUT,
-    GPIO_MODE_OUTPUT,
-    GPIO_MODE_AF,
-    GPIO_MODE_ANALOG
-};
+// struct gpio {
+//     volatile uint32_t MODER, OTYPER, OSPEEDER, PUPDR, IDR, ODR, BSRR, LCKR, AFRL,
+//     AFRH, BRR, HSLVR, SECCFGR;
+// };
 
 
-* (volatile uint32_t *) (0x42020000 + 0) = 0;	// Sets pins A0 - A15 to inputs
+// #define GPIOA ((struct gpio *) 0x42020000)
+// #define GPIO(bank) ((struct gpio *) (0x42020000 + 0x400 * (bank)))
 
-* (volatile uint32_t *) (0x42020000 + 0) &= 3 << 6;	// Clear bit ranges 6-7
-* (volatile uint32_t *) (0x42020000 + 0) |= 1 << 6;	// set bit range 6-7 to 1
+// #define BIT(x) (1UL << (x))
+// #define PIN(bank, num) ((((bank) - 'A') << 8) | (num))
+// #define PINNO(pin) (pin & 255)
+// #define PINBANK(pin) (pin >> 8) 
 
-static inline void gpio_set_mode(uint16_t pin, uint8_t mode){
-    struct gpio *gpio = GPIO(PINBANK(pin));     // GPIO bank
-    uint8_t n = PINNO(pin);                     // Pin number
-    gpio->MODER &= ~(3U << (n * 2));          // Clear existing bits
-    gpio->MODER |= (mode & 3) << (n * 2);     // Set new bits    
-}
+// enum {
+//     GPIO_MODE_INPUT,
+//     GPIO_MODE_OUTPUT,
+//     GPIO_MODE_AF,
+//     GPIO_MODE_ANALOG
+// };
 
-/*
- The Reset and Clock Control (RCC) unit is used to enable (clock) 
- peripherial buses. On STM32 MCUs, the desired peripheral must be
- enabled in order for read/ write access.
-*/
-struct rcc {
-    volatile uint32_t CR, Reserved0, ICSCR1, ICSCR2, ICSCR3, CRRCR, Reserved1,
-    CFGR1, CFGR2, CFGR3, PLL1CFGR, PLL2CFGR, PLL3CFGR, PLL1DIVR, PLL1FRACR,
-    PLL2DIVR, PLL2FRACR, PLL3DIVR, PLL3FRACR, Reserved2, CIER, CIFR, CICR,
-    Reserved3, AHB1RSTR, AHB2RSTR1, AHB2RSTR2, AHB3RSTR3, Reserved4, 
-    APB1RSTR1, APB1RSTR2, APB2RSTR, APB3RSTR, Reserved5, AHB1ENR, AHB2ENR1,
-    AHB2ENR2, AHB3ENR, APB1ENR1, APB1ENR2, APB2ENR, APB3ENR, Reserved6, 
-    AHB1SMENR, AHB2SMENR1, AHB2SMENR2, AHB3SMENR, Reserved7, APB1SMENR1, APB1SMENR2,
-    APB2SMENR, APB3SMENR, Reserved8, SRDAMR, CCIPR1, CCIPR2, CCIPR3, BDCR, CSR,
-    Reserved9[2], SECCFGR, PRIVCFGR;
-};
 
-#define RCC ((struct rcc* ) 0x46020c00) 
+// // * (volatile uint32_t *) (0x42020000 + 0) = 0;	// Sets pins A0 - A15 to inputs
+
+// // * (volatile uint32_t *) (0x42020000 + 0) &= 3 << 6;	// Clear bit ranges 6-7
+// // * (volatile uint32_t *) (0x42020000 + 0) |= 1 << 6;	// set bit range 6-7 to 1
+
+// static inline void gpio_set_mode(uint16_t pin, uint8_t mode){
+//     struct gpio *gpio = GPIO(PINBANK(pin));     // GPIO bank
+//     int n = PINNO(pin);                     // Pin number
+//     gpio->MODER &= ~(3U << (n * 2));          // Clear existing bits
+//     gpio->MODER |= (mode & 3) << (n * 2);     // Set new bits    
+// }
+
+// static inline void gpio_write(uint16_t pin, bool val){
+//     struct gpio *gpio = GPIO(PINBANK(pin));
+//     gpio->BSRR = (1u << PINNO(pin)) << (val ? 0 : 16);
+// }
+
+// static inline void spin(volatile uint32_t count){
+//     while (count--) (void) 0;
+// }
+
+// /*
+//  The Reset and Clock Control (RCC) unit is used to enable (clock) 
+//  peripherial buses. On STM32 MCUs, the desired peripheral must be
+//  enabled in order for read/ write access.
+// */
+// struct rcc {
+//     volatile uint32_t CR, Reserved0, ICSCR1, ICSCR2, ICSCR3, CRRCR, Reserved1,
+//     CFGR1, CFGR2, CFGR3, PLL1CFGR, PLL2CFGR, PLL3CFGR, PLL1DIVR, PLL1FRACR,
+//     PLL2DIVR, PLL2FRACR, PLL3DIVR, PLL3FRACR, Reserved2, CIER, CIFR, CICR,
+//     Reserved3, AHB1RSTR, AHB2RSTR1, AHB2RSTR2, AHB3RSTR3, Reserved4, 
+//     APB1RSTR1, APB1RSTR2, APB2RSTR, APB3RSTR, Reserved5, AHB1ENR, AHB2ENR1,
+//     AHB2ENR2, AHB3ENR, APB1ENR1, APB1ENR2, APB2ENR, APB3ENR, Reserved6, 
+//     AHB1SMENR, AHB2SMENR1, AHB2SMENR2, AHB3SMENR, Reserved7, APB1SMENR1, APB1SMENR2,
+//     APB2SMENR, APB3SMENR, Reserved8, SRDAMR, CCIPR1, CCIPR2, CCIPR3, BDCR, CSR,
+//     Reserved9[2], SECCFGR, PRIVCFGR;
+// };
+
+// #define RCC ((struct rcc* ) 0x46020c00) 
 
 int main(void){
+    // uint16_t led = PIN('B', 7);
+    // RCC->AHB2ENR1 |= BIT(PINBANK(led));
+    // gpio_set_mode(led, GPIO_MODE_OUTPUT);
+
+    // for(;;){
+    //     gpio_write(led, true);
+    //     spin(999999);
+    //     gpio_write(led, false);
+    //     spin(999999);
+    // }
     return 0;   // Do nothing for now
 }
 
